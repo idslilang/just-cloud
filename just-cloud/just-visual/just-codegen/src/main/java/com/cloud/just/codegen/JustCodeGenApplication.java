0354da5212474b585f78bdec0e1f362b3b1e69fb
idslilang <1170370113@qq.comgit>
2021-09-11 16:37:18 +0800

    初始化提交

---
+++ just-cloud/just-visual/just-codegen/src/main/java/com/cloud/just/codegen/JustCodeGenApplication.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020 just4cloud Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cloud.just.codegen;
+
+import com.cloud.just.common.datasource.annotation.EnableDynamicDataSource;
+import com.cloud.just.common.feign.annotation.EnableJustFeignClients;
+import com.cloud.just.common.security.annotation.EnableJustResourceServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lengleng
+ * @date 2020/03/11 代码生成模块
+ */
+@EnableDynamicDataSource
+@EnableJustFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableJustResourceServer
+public class JustCodeGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustCodeGenApplication.class, args);
+	}
+
+}
