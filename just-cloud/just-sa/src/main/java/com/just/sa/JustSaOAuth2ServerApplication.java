c7c21acb7f39df8bd864237ec40d4262d341b40a
idslilang <1170370113@qq.comgit>
2021-11-02 23:32:49 +0800

    :sparkles: 新增sa认证方式

---
+++ just-cloud/just-sa/src/main/java/com/just/sa/JustSaOAuth2ServerApplication.java
@@ -0,0 +1,18 @@
+package com.just.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: lilang
+ * @version:
+ * @modified By:1170370113@qq.com
+ */
+@SpringBootApplication
+public class JustSaOAuth2ServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JustSaOAuth2ServerApplication.class, args);
+		System.out.println("\nSa-Token-OAuth Server端启动成功");
+	}
+}
