3a64580d2e74511af924bb16bf5452dc43fd3767
idslilang <1170370113@qq.com>
2021-11-05 15:47:44 +0800

    新增oauth认证

--- just-cloud/just-common/just-common-sasecurity/src/main/java/com/cloud/just/sa/security/config/SaTokenConfigure.java
+++ just-cloud/just-common/just-common-sasecurity/src/main/java/com/cloud/just/sa/security/config/SaTokenConfigure.java
@@ -1,37 +1,33 @@
 package com.cloud.just.sa.security.config;
 
-import cn.dev33.satoken.context.SaHolder;
-import cn.dev33.satoken.filter.SaServletFilter;
-import cn.dev33.satoken.id.SaIdUtil;
+import cn.dev33.satoken.interceptor.SaRouteInterceptor;
+import cn.dev33.satoken.oauth2.logic.SaOAuth2Util;
 import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpLogic;
 import cn.dev33.satoken.stp.StpUtil;
-import cn.dev33.satoken.util.SaResult;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * Sa-Token 权限认证 配置类 
+ * Sa-Token 权限认证 配置类
  */
 @Configuration
 public class SaTokenConfigure implements WebMvcConfigurer {
-    // 注册 Sa-Token 全局过滤器 
-    @Bean
-    public SaServletFilter getSaServletFilter() {
-        return new SaServletFilter()
-                .addInclude("/**")
-                .addExclude("/favicon.ico")
-                .setAuth(obj -> {
-                    // 校验 Id-Token 身份凭证     —— 以下两句代码可简化为：SaIdUtil.checkCurrentRequestToken(); 
-                    String token = SaHolder.getRequest().getHeader(SaIdUtil.ID_TOKEN);
-                    SaIdUtil.checkToken(token);
 
-					SaRouter.match("/**", () -> StpUtil.checkLogin());
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// 注册路由拦截器，自定义认证规则
+		registry.addInterceptor(new SaRouteInterceptor((req, res, handler)->{
+			// 根据路由划分模块，不同模块不同鉴权
+			SaOAuth2Util.checkAccessToken(StpUtil.getTokenValue());
+			SaRouter.match("/user/**", r -> StpUtil.checkPermission("user"));
+			SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
+			SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
+			SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
+			SaRouter.match("/notice/**", r -> StpUtil.checkPermission("notice"));
+			SaRouter.match("/comment/**", r -> StpUtil.checkPermission("comment"));
+		})).addPathPatterns("/**");
+	}
 
-				})
-                .setError(e -> {
-                    return SaResult.error(e.getMessage());
-                })
-                ;
-    }
 }
