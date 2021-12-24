772cf4eabcfff3bfcceab6d723c246e27d5ea7e1
idslilang <1170370113@qq.com>
2021-12-24 10:54:09 +0800

    :sparkles:  网关接入灰度

--- just-cloud/just-gateway/src/main/java/com/cloud/just/gateway/gray/GrayRandomLoadBalancerConfiguration.java
+++ just-cloud/just-gateway/src/main/java/com/cloud/just/gateway/gray/GrayRandomLoadBalancerConfiguration.java
@@ -1,19 +1,15 @@
 package com.cloud.just.gateway.gray;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientConfiguration;
 import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 
-@Configuration
-@ConditionalOnProperty(value = "gateway.gray.enabled", havingValue = "true")
 public class GrayRandomLoadBalancerConfiguration extends LoadBalancerClientConfiguration {
 
 
