03d707a298cf03e24a99ec4fc4386dd6ca864445
idslilang <1170370113@qq.com>
2021-12-24 10:34:35 +0800

    :sparkles:  网关接入灰度

--- just-cloud/just-gateway/src/main/java/com/cloud/just/gateway/config/GrayConfig.java
+++ just-cloud/just-gateway/src/main/java/com/cloud/just/gateway/config/GrayConfig.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.context.annotation.Configuration;
 
+
+/**
+ * LoadBalancerClients 动态加载不同的配置类 https://segmentfault.com/a/1190000040082624
+ * 通过使用 @LoadBalancerClients 和 @LoadBalancerClient 注解可以自动生成对应的 LoadBalancerClientSpecification 进而实现公共负载均衡配置或者特定某个微服务的负载均衡配置。
+ */
 @Configuration
 @LoadBalancerClients(defaultConfiguration = GrayReactorLoadBalancerConfiguration.class)
 @ConditionalOnProperty(value = "gateway.gray.enabled", havingValue = "true")
