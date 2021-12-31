055f93b698a51cd958878ee379ef0d3834d64d35
idslilang <1170370113@qq.com>
2021-12-31 10:11:13 +0800

    :sparkles:  修改注释

--- just-cloud/just-common/just-common-core/src/main/java/com/cloud/just/common/core/constant/CacheConstants.java
+++ just-cloud/just-common/just-common-core/src/main/java/com/cloud/just/common/core/constant/CacheConstants.java
@@ -1,25 +1,6 @@
-/*
- * Copyright (c) 2020 just4cloud Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.cloud.just.common.core.constant;
 
 /**
- * @author lengleng
- * @date 2020年01月01日
- * <p>
  * 缓存的key 常量
  */
 public interface CacheConstants {
